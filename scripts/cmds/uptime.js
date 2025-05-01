b9c6789735715c49fb8e3b2cd965fe9c1dfe8832
jonatha23456789 <159666966+jonatha23456789@users.noreply.github.com>
2025-05-01 03:51:28 -0400

    Update uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -23,6 +23,6 @@ module.exports = {
     const hours = Math.floor((uptime / (60 * 60)) % 24);
     const days = Math.floor(uptime / (60 * 60 * 24));
     const uptimeString = `${hours} hours ${minutes} minutes ${seconds} second`;
-    api.sendMessage(`🧑‍🏫|𝙃𝙄 𝙈𝘼𝙎𝙏𝙀𝙍 𝗛𝗜𝗞𝗔𝗥𝗨📑 \n___________________\n, 🎀 𝗟𝗢𝗩𝗘𝗟𝗬 𝗛𝘼𝙎 𝘽𝙀𝙀𝙉 𝙍𝙐𝙉𝙄𝙉𝙂 𝙁𝙊𝙍 \n____________________\n [⏰${uptimeString}]\n\n______________________🧛𝘿𝙍𝘼𝘾𝙐𝙇𝘼🧛.`, event.threadID);
+    api.sendMessage(`🧑‍🏫|𝙃𝙄 𝙈𝘼𝙎𝙏𝙀𝙍 𝗛𝗜𝗞𝗔𝗥𝗨📑 \n___________________\n, 🎀 𝗟𝗢𝗩𝗘𝗟𝗬 𝗛𝘼𝙎 𝘽𝙀𝙀𝙉 𝙍𝙐𝙉𝙄𝙉𝙂 𝙁𝙊𝙍 \n____________________\n [⏰${uptimeString}]\n\n______________________🎀 𝗟𝗢𝗩𝗘𝗟𝗬 🎀 .`, event.threadID);
   }
 };
